dd8d60d7815628d5a06c319350315b5dd7d399be
Pavel <grigorov_ps@mail.ru>
2023-09-29 22:01:11 +0300

    Создали проект Example005_if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "паша") {
+    Console.WriteLine("УРА! это же ПАША!");
+}
+else {
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
