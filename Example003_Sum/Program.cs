337c29064a1c414734529dc3f1f82b0dbfc11427
Pavel <grigorov_ps@mail.ru>
2023-09-29 14:56:23 +0300

    Внесли изменения в проект Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numA = 5;
-int numB = 4;
+﻿int numA = new Random ().Next (1,10); // 1 2 3 4 ... 9
+Console.WriteLine (numA);
+int numB = new Random ().Next (1,10);
+Console.WriteLine (numB);
 int numC = 12;
 int result = (numA + numC);
 Console.WriteLine (numA + numB);
