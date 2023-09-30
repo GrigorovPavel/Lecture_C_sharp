36fe70815af2375e4aea66ca2ef54a1788b5ea9f
Pavel <grigorov_ps@mail.ru>
2023-09-30 09:49:39 +0300

    Создали программу Example006_Base - (Нахождение наибольшего значения).

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int numA = 1;
+int numB = 8;
+int numC = 3;
+int numD = 2;
+int numE = 6;
+int MAX = numA;
+if (numA > MAX) MAX =numA;
+if (numB > MAX) MAX =numB;
+if (numC > MAX) MAX =numC;
+if (numD > MAX) MAX =numD;
+if (numE > MAX) MAX =numE;
+
+Console.Write ("MAX = ");
+Console.WriteLine (MAX);
